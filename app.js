dd4d1304d53d1db22e1e6fe73f32290f69100868
Douglas Keeble <douglaskeeble@gmail.com>
2018-09-18 02:27:55 +0000

    Added env variable for db

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     authRoutes = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
-mongoose.connect("mongodb://doug:1question@ds161312.mlab.com:61312/yelpcamp", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
